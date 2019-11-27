a0ba1bc4ea908d250e584e1e1ae1674993bbe12f
Charles Prevot <charles.prevot8@gmail.com>
2019-11-27 12:28:50 +0100

    [QUARANTINE] add timeout & exception

--- forti_add_quarantine.py
+++ forti_add_quarantine.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Description: Python script to quarantine a host in the security fabric
+# Description: Python script to quarantine a host in the security fabric. IP will be visible in menu Monitor > Quarantine Monitor.
 # Date: 07 nov 2019
+# Update: 27 nov 2019
 # Authors: Charles Prevot at Fortinet Paris
 # Requirements: pip install requests
 
@@ -27,38 +28,43 @@ def getError(code: int):
 
 class Fortigate(object):
 
-    def __init__(self, fgt_ip, port, token, vdom="root", verify=False):
+    def __init__(self, fgt_ip, port, token, vdom="root", verify=False, timeout=10):
         self.ip = fgt_ip # IP or Hostname FortiGate
         self.port = port # Administrative port for https
         self.token = token # API token
 
-        self.url = "https://{}:{}".format(self.ip, str(self.port))
+        self.url = "https://{ip}:{port}".format(ip=self.ip, port=str(self.port))
         self.headers = {"Authorization": "Bearer " + self.token}
         self.vdom = vdom
 
         self.verify = verify
+        self.timeout = timeout
 
     def add_quarantine(self, quarantine_host):
         data = {
             "ip_addresses": [ str(quarantine_host) ],
             "expiry": 0
         }
-        res = requests.post(self.url + '/api/v2/monitor/user/banned/add_users',
-            headers=self.headers, data=json.dumps(data), params={"vdom": self.vdom}, verify=self.verify)
+        try:
+            res = requests.post(self.url + '/api/v2/monitor/user/banned/add_users',
+                headers=self.headers, data=json.dumps(data), params={"vdom": self.vdom}, timeout=self.timeout, verify=self.verify)
+        except:
+            raise
+
         return res
 
 
 def main():
-    fgt = Fortigate("10.200.3.1", 1443, "cjk8f01gs0037j06djjckskkG55GGh")
+    fgt = Fortigate("x.x.x.x", 443, "token")
 
     try:
-        response = fgt.add_quarantine("192.168.1.10")
+        response = fgt.add_quarantine("1.1.1.1")
         getError(response.status_code)
     except Exception as e:
-        print(e.args[1])
+        print(str(e.args))
         return 1
 
-    print("Successfully added address")
+    print("Successfully added quarantine host")
     return 0
 
 
